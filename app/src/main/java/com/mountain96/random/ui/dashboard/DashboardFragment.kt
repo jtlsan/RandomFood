b9bd55affea13cefd531be9ea10a2833deb6455d
San <jtlsan@naver.com>
2020-09-06 21:47:54 +0900

    feature:Update activity_main to use fragment

--- app/src/main/java/com/mountain96/random/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/mountain96/random/ui/dashboard/DashboardFragment.kt
@@ -21,11 +21,14 @@ class DashboardFragment : Fragment() {
     ): View? {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_list, container, false)
+        val root = inflater.inflate(R.layout.fragment_foods, container, false)
+        /*
         val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+         */
         return root
     }
 }
