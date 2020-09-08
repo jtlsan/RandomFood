4bd12fa2ca649be29ba22b01a9e4ff9134399b83
San <jtlsan@naver.com>
2020-09-08 19:04:35 +0900

    feature: Update FoodDTO

--- app/src/main/java/com/mountain96/random/model/FoodDTO.kt
+++ app/src/main/java/com/mountain96/random/model/FoodDTO.kt
@@ -3,4 +3,5 @@ package com.mountain96.random.model
 data class FoodDTO (var name : String? = null,
                     var category : FoodCategory? = null,
                     var image  : String? = null,
-                    var isChecked : Boolean? = null)
+                    var isChecked : Boolean? = null,
+                    var isFavorite : Boolean? = null)
